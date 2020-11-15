f697015eab36a0e735dffcc3d94c8f771662e8e6
shahinul  islam suzon <suzonice15@gmail.com>
2020-11-15 07:38:14 +0600

    Top banner component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TopBanner from "./components/TopBanner/TopBanner";
+
+
+function App() {
+  return (
+    <div>
+<TopBanner/>
+
+    </div>
+  );
+}
+
+export default App;
